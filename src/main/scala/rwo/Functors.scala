1d1d9d52732f7f4148fd81b9a8480bc0fb9072dd
Alan O'Donnell <alan.m.odonnell@gmail.com>
2013-10-29 09:56:41 -0400

    Trivial example with structural types

--- src/main/scala/rwo/Functors.scala
+++ src/main/scala/rwo/Functors.scala
@@ -1,6 +1,7 @@
 package rwo
 
 import scala.language.higherKinds
+import scala.language.reflectiveCalls
 
 /*
  * "A trivial example", c.f. Chapter 9.
@@ -9,12 +10,22 @@ object ATrivialExample {
   trait XIntSig {
     def x : Int
   }
-
   def increment(M : XIntSig) : XIntSig = new XIntSig {
     def x = M.x + 1
   }
-  
   val Three = new XIntSig {
+    def x = 3
+  }
+  val Four = increment(Three)
+  val hopefullyOne = Four.x - Three.x
+}
+
+object ATrivialExampleWithStructuralTypes {
+  type XIntSig = { def x : Int }
+  def increment(M : XIntSig) : XIntSig = new {
+    def x = M.x + 1
+  }
+  val Three : XIntSig = new {
     def x = 3
   }
   val Four = increment(Three)
